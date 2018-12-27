898eaae609ae1ccc84609ef541fd3c06d6ae36ea
lan-qing <tiandiwangshen@icloud.com>
2018-12-28 02:20:27 +0800

    text

--- text.js
+++ text.js
@@ -1,9 +1,12 @@
 quotes = [
     'Are you finding dark from light, or finding light from dark?',
     'All is good. Nothing is best.',
+    'Someday I will just like you! Me!',
+    'Do you want to play with fire?',
     '落叶完成了最后的颤抖。',
     '未觉池塘春草梦，阶前梧叶已秋声。',
     '当时明月在，曾照彩云归。',
+    '你在凝望深渊时，深渊也在凝望你。',
 ];
 var index = Math.floor(Math.random() * quotes.length);
 var quote = quotes[index];
