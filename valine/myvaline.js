e1a6a03af5e2b07a995d5281c918fbe556125065
lan-qing <tiandiwangshen@icloud.com>
2018-12-28 02:58:00 +0800

    test valine

--- valine/myvaline.js
+++ valine/myvaline.js
@@ -1,4 +1,5 @@
-new Valine({av: AV,
+new Valine({
+    av: AV,
     el: '.comment',
     emoticon_url: 'https://cloud.panjunwen.com/alu',
     emoticon_list: ["吐.png","期待.png","高兴.png","吐血倒地.png","哭泣.png","欢呼.png"],
