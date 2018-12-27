51a5ba3955a581f24b44cdf1a1be09d9ec3ef1f0
lan-qing <tiandiwangshen@icloud.com>
2018-12-28 02:04:52 +0800

    text

---
+++ text.js
@@ -0,0 +1,10 @@
+quotes = [
+    'Are you finding dark from light, or finding light from dark?',
+    'All is good. Nothing is best.',
+    '落叶完成了最后的颤抖。',
+    '未觉池塘春草梦，阶前梧叶已秋声。',
+    '当时明月在，曾照彩云归。',
+];
+var index = Math.floor(Math.random() * quotes.length);
+var quote = quotes[index];
+document.write(quote);
