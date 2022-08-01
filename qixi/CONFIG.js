874c21ecc43f58e78a34ff72f904d4d35b93544e
jiaruzhang <jiaruzhang@sjtu.edu.cn>
2022-08-01 13:36:47 +0800

    qixi

---
+++ qixi/CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "棒棒我爱你！",
+  descriptionHeader:
+    "你愿意和我相爱到永远吗？",
+  buttonYes: "我愿意！",
+  buttonNo: "对不起！我不愿意。",
+  titleModar: "我也愿意！",
+  descriptionModar:
+    ' 这世上美好的东西不太多，立秋傍晚从河对岸吹来的风，二十来岁笑起来要人命的你。 "',
+};
