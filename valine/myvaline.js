c5d9d851412b06d62a8693743beeb41c10a54273
jiaruzhang <jiaruzhang@sjtu.edu.cn>
2023-09-20 17:12:15 +0800

    new

--- valine/myvaline.js
+++
@@ -1,9 +0,0 @@
-new Valine({
-    av: AV,
-    el: '.comment',
-    emoticon_url: 'https://cloud.panjunwen.com/alu',
-    emoticon_list: ["吐.png","期待.png","高兴.png","吐血倒地.png","哭泣.png","欢呼.png"],
-    app_id: '7X9T4xSYpt3QtsjLoaUN4Doj-gzGzoHsz',
-    app_key: 'VpskNYOAiEsrNBc0ADwxlTPs',
-    placeholder: '在这输入评论',
-});
